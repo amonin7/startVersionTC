b0ad17980833ce50e49a127142f288ce189c1fdd
Andrey Minin <ewkjf@mail.ru>
2019-02-10 20:51:18 +0300

    1st commit

--- startVirsionTC/AppDelegate.swift
+++ startVirsionTC/AppDelegate.swift
@@ -13,34 +13,63 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    let ifShowMessages: Bool = true
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        if (ifShowMessages) {
+            print("Application moved from Not running to Inactive state")
+            print(#function)
+            print("\n")
+        }
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        if (ifShowMessages) {
+            print("Application moved from Active to Inactive state")
+            print(#function)
+            print("\n")
+        }
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        if (ifShowMessages) {
+            print("Application moved from Inactive to Background state")
+            print(#function)
+            print("\n")
+        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        if (ifShowMessages) {
+            print("Application moved from Background to Foreground (to Inactive) state")
+            print(#function)
+            print("\n")
+        }
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        if (ifShowMessages) {
+            print("Application moved from Inactive to Active state")
+            print(#function)
+            print("\n")
+        }
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
+        if (ifShowMessages) {
+            print("Application is closed properly (moved from Suspended to Not running)")
+            print(#function)
+            print("\n")
+        }
         self.saveContext()
     }
 
--- startVirsionTC/ViewController.swift
+++ startVirsionTC/ViewController.swift
@@ -9,12 +9,85 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let ifShowVCMessages: Bool = true
+    
+    override func awakeFromNib() {
+        if (ifShowVCMessages) {
+            print(#function)
+            print("\n")
+        }
+        super.awakeFromNib()
+    }
+    
+    // operates before view will appear on screen
+    override func viewWillAppear(_ animated: Bool) {
+        if (ifShowVCMessages) {
+            print(#function)
+            print("\n")
+        }
+        super.viewWillAppear(true)
+    }
+    
+    // operates before size of view will change to size of screen
+    override func viewWillLayoutSubviews() {
+        if (ifShowVCMessages) {
+            print(#function)
+            print("\n")
+        }
+        super.viewWillLayoutSubviews()
+    }
+    
+    // operates after size of view is changed to size of screen
+    override func viewDidLayoutSubviews() {
+        if (ifShowVCMessages) {
+            print(#function)
+            print("\n")
+        }
+        super.viewDidLayoutSubviews()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if (ifShowVCMessages) {
+            print(#function)
+            print("\n")
+        }
+        super.viewDidAppear(true)
+    }
+    
     override func viewDidLoad() {
+        if (ifShowVCMessages) {
+            print(#function)
+            print("\n")
+        }
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // operates before the view is closed
+    override func viewWillDisappear(_ animated: Bool) {
+        if (ifShowVCMessages) {
+            print(#function)
+            print("\n")
+        }
+        super.viewWillDisappear(true)
+    }
 
+    // operates after the  view is closed
+    override func viewDidDisappear(_ animated: Bool) {
+        if (ifShowVCMessages) {
+            print(#function)
+            print("\n")
+        }
+        super.viewDidDisappear(true)
+    }
+    
+    // upload object from memory
+    deinit {
+        if (ifShowVCMessages) {
+            print(#function)
+            print("\n")
+        }
+    }
+    
 }
 
